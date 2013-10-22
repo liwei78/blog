081a2aa0e58b4d5f606f2b4eecca350bb8d6b847
liwei <hi@liwei.me>
2013-10-22 20:13:15 +0800

    fix for permalink

--- lib/tool.rb
+++ lib/tool.rb
@@ -1,5 +1,14 @@
-require "jekyll-import"
-JekyllImport::WordpressDotCom.process({ :source => File.join('..', '_backup', "wordpress.xml") })
-
-
-# ruby -rubygems -e 'require "jekyll/jekyll-import/wordpressdotcom";JekyllImport::WordpressDotCom.process({ :source => "_backup/wordpress.xml" })'
+post_path = File.join(ENV["PWD"], '_posts')
+Dir.glob("#{post_path}/*.textile") do |filename|
+  new_file = []
+  IO.readlines(filename).each do |line|
+    next if line.match(/^permalink: [\S]+/)
+    new_file << line
+  end
+  new_file.each 
+  File.open(filename, 'w+') do |f|
+    new_file.each do |line|
+      f.line = line
+    end
+  end
+end
